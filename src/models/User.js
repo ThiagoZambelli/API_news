9563c0019d735be01368b13c445ced145fe6b0a8
Thiago Zambelli Oliveira <90356970+ThiagoZambelli@users.noreply.github.com>
2026-05-08 13:46:51 +0000

    refactor: implementação de criptografia para senhas de usuarios

--- src/models/User.js
+++ src/models/User.js
@@ -1,25 +1,31 @@
 //Model que controla o modelo que vai conectar com o DB
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true
+        required: true
     },
     userName: {
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
     email: {
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
     password: {
         type: String,
-        require: true
+        required: true,
+        select: false
     }
+});
+
+UserSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model("User", UserSchema);

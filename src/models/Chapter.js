9d59772f2ffdf3a2c317f59e9e0d76c680b5d8d5
Thiago Zambelli Oliveira <90356970+ThiagoZambelli@users.noreply.github.com>
2026-05-14 13:03:30 +0000

    feat: create chapter schema

---
+++ src/models/Chapter.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const Chapter = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    text: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    }
+});
+
+const Chapter = mongoose.model("Chapter", ChapterSchema);
+
+export default Chapter;
